e8fdfaf2db39743d54759f148b8938fe0dc67ae9
RuslanShugaipov <nabon1812@mail.ru>
2022-04-27 18:16:06 +0300

    Добавьте файлы проекта.

---
+++ Lab_4/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(100, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 320);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.Location = new System.Drawing.Point(460, 50);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(120, 56);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0 отрезков";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Отсечь";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 433);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab_4/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_4
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        PointF[] points;
+
+        Pen pen_black = new Pen(Color.Black);
+        Pen pen_blue = new Pen(Color.Blue);
+        List<int> outer = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Invalidate();
+
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+
+            int k = trackBar1.Value * 2;
+
+            points = new PointF[k];
+
+            var rand = new Random();
+
+            for (int i = 0; i < k; ++i)
+            {
+                points[i].X = rand.Next(w + 1);
+                points[i].Y = rand.Next(h + 1);
+            }
+
+            if (k != 0)
+            {
+                g.DrawRectangle(pen_blue, new Rectangle(70, 80, 100, 100));
+            }
+
+            for (int i = 0; i < k - 1; i += 2)
+            {
+                g.DrawLine(pen_black, points[i], points[i + 1]);
+            }
+
+            label1.Text = $"{k / 2} отрезков";
+        }
+
+        int get_code(PointF point)
+        {
+            int code = 0b0000;
+            code |= point.X < 70 ? 0b0001 : 0b0000;
+            code |= point.X > 170 ? 0b0010 : 0b0000;
+            code |= point.Y < 80 ? 0b1000 : 0b0000;
+            code |= point.Y > 180 ? 0b0100 : 0b0000;
+            return code;
+        }
+
+        int clip_segment(int i)
+        {
+            int code_a = get_code(points[i]);
+            int code_b = get_code(points[i + 1]);
+            int code;
+
+            float x, y;
+
+            PointF temp;
+
+            while ((code_a | code_b) != 0b0000)
+            {
+                if ((code_a & code_b) != 0b0000)
+                {
+                    return 2;
+                }
+
+                if (code_a != 0b0000)
+                {
+                    code = code_a;
+                    temp = points[i];
+                }
+                else
+                {
+                    code = code_b;
+                    temp = points[i + 1];
+                }
+
+                if ((code & 0b0001) != 0b0000)
+                {
+                    temp.Y += (points[i].Y - points[i + 1].Y) * (70 - temp.X) / (points[i].X - points[i + 1].X);
+                    temp.X = 70;
+                }
+                else if ((code & 0b0010) != 0b0000)
+                {
+                    temp.Y += (points[i].Y - points[i + 1].Y) * (170 - temp.X) / (points[i].X - points[i + 1].X);
+                    temp.X = 170;
+                }
+                else if ((code & 0b0100) != 0b0000)
+                {
+                    temp.X += (points[i].X - points[i + 1].X) * (180 - temp.Y) / (points[i].Y - points[i + 1].Y);
+                    temp.Y = 180;
+                }
+                else if ((code & 0b1000) != 0b0000)
+                {
+                    temp.X += (points[i].X - points[i + 1].X) * (80 - temp.Y) / (points[i].Y - points[i + 1].Y);
+                    temp.Y = 80;
+                }
+
+                if(code == code_a)
+                {
+                    points[i] = temp;
+                    code_a = get_code(points[i]);
+                }
+                else
+                {
+                    points[i + 1] = temp;
+                    code_b = get_code(points[i + 1]);
+                }
+            }
+            return 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (points == null)
+                return;
+            else if (points.Length == 0)
+                return;
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Invalidate();
+
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+
+            for (int i = 0; i < points.Length - 1; i += 2)
+            {
+                switch (clip_segment(i))
+                {
+                    case 1:
+                        continue;
+                    case 2:
+                        outer.Add(i);
+                        break;
+                }
+            }
+
+            g.DrawRectangle(pen_blue, new Rectangle(70, 80, 100, 100));
+
+            for (int i = 0; i < points.Length - 1; i += 2)
+            {
+                if (outer.Contains(i))
+                {
+                    continue;
+                }
+                g.DrawLine(pen_black, points[i], points[i + 1]);
+            }
+
+            outer.Clear();
+        }
+    }
+}
